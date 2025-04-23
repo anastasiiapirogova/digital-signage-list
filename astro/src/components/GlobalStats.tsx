26766e2726ef4d88e6b41f8c9173929c7116e8b9
514sid <140138716+514sid@users.noreply.github.com>
2025-04-24 00:29:59 +0400

    adjust padding in GlobalStats component for improved layout

--- astro/src/components/GlobalStats.tsx
+++ astro/src/components/GlobalStats.tsx
@@ -3,7 +3,7 @@ import { ResetFiltersButton } from "./ResetFiltersButton";
 
 export const GlobalStats = () => {
     return (
-        <div className="flex gap-10 items-center w-full px-10 font-mono mb-5 h-10">
+        <div className="flex gap-10 items-center w-full px-5 md:px-10 font-mono mb-5 h-10">
             <FilteredProductsCount />
             <ResetFiltersButton />
         </div>
