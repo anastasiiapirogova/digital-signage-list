739b870eeae28c629b74858dabfe08e3087bee97
514sid <140138716+514sid@users.noreply.github.com>
2025-04-23 22:07:46 +0400

    refactor FiltersSidebar layout for improved structure and styling

--- astro/src/components/FiltersSidebar.tsx
+++ astro/src/components/FiltersSidebar.tsx
@@ -7,11 +7,11 @@ import { PricingFilter } from "./PricingFilter";
 
 export const FiltersSidebar = () => {
     return (
-        <div className="w-[300px] flex flex-col gap-5 max-h-screen overflow-y-auto sticky top-0 scrollbar-thin p-5 bg-neutral-100">
+        <>
             <PlatformFilter />
             <PricingFilter />
             <OpenSourceFilter />
             <HeadquartersFilter />
-        </div>
+        </>
     )
 }
