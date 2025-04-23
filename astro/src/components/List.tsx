5075e192f8272187f915ea6a3939d5d2e10b1652
514sid <140138716+514sid@users.noreply.github.com>
2025-04-24 00:33:34 +0400

    formatting

--- astro/src/components/List.tsx
+++ astro/src/components/List.tsx
@@ -1,31 +1,31 @@
-import { useEffect, useState } from 'react';
-import { filterProducts } from '../utils/filterProducts';
-import { products } from '../utils/products';
-import { ListItem } from './ListItem';
-import { useStore } from '@nanostores/react';
-import { productFiltersStore, setProductFilters } from '../utils/productFiltersStore';
+import { useEffect, useState } from 'react'
+import { filterProducts } from '../utils/filterProducts'
+import { products } from '../utils/products'
+import { ListItem } from './ListItem'
+import { useStore } from '@nanostores/react'
+import { productFiltersStore, setProductFilters } from '../utils/productFiltersStore'
 
 export const List = () => {
-    const $filters = useStore(productFiltersStore);
+    const $filters = useStore(productFiltersStore)
 
     const [filteredProducts, setFilteredProducts] = useState(() =>
         filterProducts(products, $filters)
-    );
+    )
 
     useEffect(() => {
-        const newFilteredProducts = filterProducts(products, $filters);
-        setFilteredProducts(newFilteredProducts);
-    }, [$filters]);
+        const newFilteredProducts = filterProducts(products, $filters)
+        setFilteredProducts(newFilteredProducts)
+    }, [$filters])
 
     useEffect(() => {
-        const filters: Record<string, string> = {};
+        const filters: Record<string, string> = {}
 
         new URLSearchParams(window.location.search).forEach((value, key) => {
-            filters[key] = value;
-        });
+            filters[key] = value
+        })
 
-        setProductFilters(filters);
-    }, []);
+        setProductFilters(filters)
+    }, [])
 
     return (
         <div className='flex flex-col w-full gap-2'>
@@ -33,5 +33,5 @@ export const List = () => {
                 <ListItem product={product} key={product.id} />
             ))}
         </div>
-    );
-};
+    )
+}
