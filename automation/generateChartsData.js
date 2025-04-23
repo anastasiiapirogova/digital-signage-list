f952bfce42dbde06432a67013712283bbbb29560
514sid <140138716+514sid@users.noreply.github.com>
2025-04-23 21:40:25 +0400

    add script to generate charts data for products by year and headquarters

---
+++ automation/generateChartsData.js
@@ -0,0 +1,57 @@
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const productsFilePath = join(__dirname, '../data/products.json');
+const productsData = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+function getProductsFoundedByYear(products) {
+    const counts = products.reduce((acc, product) => {
+        const year = product.year_founded;
+        if (year) {
+            acc[year] = (acc[year] || 0) + 1;
+        }
+        return acc;
+    }, {});
+
+    const years = Object.keys(counts).map(Number);
+    const minYear = Math.min(...years);
+    const maxYear = new Date().getFullYear();
+
+    const result = {};
+    for (let year = minYear; year <= maxYear; year++) {
+        result[year] = counts[year] || 0;
+    }
+
+    return result;
+}
+
+const productsFoundedByYear = getProductsFoundedByYear(productsData);
+
+const productsFoundedByYearOutputFilePath = join(__dirname, '../data/charts/productsFoundedByYear.json');
+
+fs.writeFileSync(productsFoundedByYearOutputFilePath, JSON.stringify(productsFoundedByYear, null, 2), 'utf-8');
+
+console.log('Products founded by year has been generated');
+
+function getProductsByHeadquarters(products) {
+    const counts = products.reduce((acc, product) => {
+        const headquarters = product.headquarters;
+        if (headquarters && headquarters !== "Unknown") {
+            acc[headquarters] = (acc[headquarters] || 0) + 1;
+        }
+        return acc;
+    }, {});
+
+    return counts;
+}
+
+const productsByHeadquarters = getProductsByHeadquarters(productsData);
+
+const productsByHeadquartersOutputFilePath = join(__dirname, '../data/charts/productsByHeadquarters.json');
+
+fs.writeFileSync(productsByHeadquartersOutputFilePath, JSON.stringify(productsByHeadquarters, null, 2), 'utf-8');
+
+console.log('Products by headquarters has been generated');
