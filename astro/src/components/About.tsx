db421114c1604c838b3231c2a0691da0fba9847e
514sid <140138716+514sid@users.noreply.github.com>
2025-04-24 00:27:22 +0400

    enhance About component with support message and improved layout

--- astro/src/components/About.tsx
+++ astro/src/components/About.tsx
@@ -1,7 +1,12 @@
 export const About = () => {
   return (
-    <div className="px-5 md:px-10 text-3xl h-[240px] flex items-center">
+    <div className="px-5 md:px-10 h-[240px] flex flex-col justify-center gap-5">
+      <div className="text-3xl"> 
       A curated list of digital signage software
+      </div>
+      <div>
+      ⭐ Support our work - <a href="https://github.com/514sid/digital-signage-list" className="text-blue-600 hover:underline">star the project on GitHub!</a>
+      </div>
     </div>
   )
 }
