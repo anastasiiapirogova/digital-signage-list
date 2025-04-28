f7c7cff6cd354a659f89854013dc8ca13819d949
514sid <140138716+514sid@users.noreply.github.com>
2025-04-29 00:46:24 +0400

    style: improve layout structure in GlobalStats component

--- astro/src/components/GlobalStats.tsx
+++ astro/src/components/GlobalStats.tsx
@@ -4,12 +4,14 @@ import { SortOptions } from "./SortOptions";
 
 export const GlobalStats = () => {
     return (
-        <div className="flex items-center justify-between w-full px-5 md:px-10 font-mono mb-5 h-10">
+        <div className="flex flex-col gap-5 md:gap-0 md:flex-row md:items-center md:justify-between w-full px-5 md:px-10 font-mono mb-5 min-h-10">
             <div className="flex gap-10 items-center">
                 <FilteredProductsCount />
                 <ResetFiltersButton />
             </div>
-            <SortOptions />
+            <div>
+                <SortOptions />
+            </div>
         </div>
     )
 }
