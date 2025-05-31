24fd86781ed2b991050143d31bc70aace93b6486
514sid <140138716+514sid@users.noreply.github.com>
2025-05-31 08:53:58 +0400

    refactor: simplify path resolution for product and README file

--- automation/generateReadmeProductList.js
+++ automation/generateReadmeProductList.js
@@ -2,10 +2,10 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const productsFilePath = path.resolve(__dirname, '../data/products.json');
-const readmeFilePath = path.resolve(__dirname, '../README.MD');
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const productsFilePath = path.join(__dirname, '..', 'data', 'products.json');
+const readmeFilePath = path.join(__dirname, '..', 'README.MD');
 
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
