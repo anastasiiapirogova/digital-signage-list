4de33d750cb11ee0247c3e70fb51d8074164bb27
514sid <140138716+514sid@users.noreply.github.com>
2025-07-11 18:50:34 +0400

    Refactor automation script to replace JavaScript commands with TypeScript implementations, enhancing maintainability and consistency in command execution.

--- automation/run.js
+++ automation/run.js
@@ -1,31 +1,29 @@
-import { exec } from 'child_process';
+import { exec } from 'child_process'
 
 const commands = [
-    'node automation/updatePlatforms.js',
-    'node automation/generateLogos.js',
-    'node automation/updateLogoInformation.js',
-    'node automation/generatePriceRank.js',
-    'node automation/mergeProducts.js',
-    'node automation/generateReadmeProductList.js',
-    'node automation/generateChartsData.js',
-    'node automation/generatePlansCsv.js',
-];
+	'node automation/updatePlatforms.js',
+	'node automation/generateLogos.js',
+	'node automation/updateLogoInformation.js',
+	'npx tsx automation/mergeProducts.ts',
+	'npx tsx automation/generateReadmeProductList.ts',
+	'npx tsx automation/generateChartsData.ts',
+]
 
 function runCommands(commands) {
-    if (commands.length === 0) return;
+	if (commands.length === 0) return
 
-    const command = commands.shift();
-    exec(command, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error executing ${command}:`, error.message);
-            return;
-        }
-        if (stderr) {
-            console.error(`Stderr from ${command}:`, stderr);
-        }
-        console.log(`${stdout}`);
-        runCommands(commands);
-    });
+	const command = commands.shift()
+	exec(command, (error, stdout, stderr) => {
+		if (error) {
+			console.error(`Error executing ${command}:`, error.message)
+			return
+		}
+		if (stderr) {
+			console.error(`Stderr from ${command}:`, stderr)
+		}
+		console.log(`${stdout}`)
+		runCommands(commands)
+	})
 }
 
-runCommands(commands.slice());
+runCommands(commands.slice())
